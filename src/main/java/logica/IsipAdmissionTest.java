374fb4dec55b208d79e7daaa7a855e0c70f67f8c
roycvx <royelias-227-@hotmail.com>
2025-12-04 23:42:49 -0500

    chore: initial commit

---
+++ src/main/java/logica/IsipAdmissionTest.java
@@ -0,0 +1,13 @@
+
+package logica;
+
+/**
+ *
+ * @author royel
+ */
+public class IsipAdmissionTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
